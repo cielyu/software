825adb22bab7d69eaab44fc4523829f4414e9705
大婶 <543418217@qq.com>
2015-10-06 16:01:06 +0800

    add iOS project

---
+++ iOS/guahao/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  guahao
+//
+//  Created by Jeff Wong on 15/10/6.
+//  Copyright © 2015年 Jeff. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.whiteColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
